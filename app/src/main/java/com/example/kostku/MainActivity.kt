7a348df8efc3fc66506e79f130ad7afe1eb89bc0
Rachmad Yoga Saputra <yogas910@gmail.com>
2023-10-25 14:11:11 +0700

    Menambahkan firebase Authentication SDK

--- app/src/main/java/com/example/kostku/MainActivity.kt
+++ app/src/main/java/com/example/kostku/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.kostku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
