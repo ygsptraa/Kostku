709678e54f41390f32698ee41ab2149b3f7fcc75
adityputr <adityputr45@gmail.com>
2023-10-31 14:49:05 +0700

    Menambahkan binding pada Main Activity dan memanggil fragment

--- app/src/main/java/com/example/kostku/MainActivity.kt
+++ app/src/main/java/com/example/kostku/MainActivity.kt
@@ -2,12 +2,41 @@ package com.example.kostku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.kostku.databinding.ActivityLoginBinding
+import com.example.kostku.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.favorit -> replaceFragment(FavoritFragment())
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profil -> replaceFragment(ProfilFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
