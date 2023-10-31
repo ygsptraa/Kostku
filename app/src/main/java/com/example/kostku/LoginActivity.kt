3eb58da6987ff5ccdbfc267d11af0014eee48343
Rachmad Yoga Saputra <yogas910@gmail.com>
2023-10-31 08:50:15 +0700

    Menambahkan click listener untuk register pada activity_login.xml

--- app/src/main/java/com/example/kostku/LoginActivity.kt
+++ app/src/main/java/com/example/kostku/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.kostku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kostku.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        //Menambahkan clicklistener untuk register
+        binding.TvToSign.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
