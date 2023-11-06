b7ad31fe82b90258c1564b8bd0c1d6fd9765464e
adityputr <adityputr45@gmail.com>
2023-11-06 22:13:34 +0700

    Menambahkan ViewModel pada UserViewModel

--- app/src/main/java/com/example/kostku/Model/UserViewModel.kt
+++ app/src/main/java/com/example/kostku/Model/UserViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.kostku.Model
 
-class UserViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kostku.Repository.UserRepository
+
+class UserViewModel : ViewModel() {
+
+    private val repository : UserRepository
+    private val _allKost = MutableLiveData<List<User>>()
+    val allKost : LiveData<List<User>> = _allKost
+
+    init {
+        repository = UserRepository().getInstance()
+        repository.loadKost(_allKost)
+    }
 }
