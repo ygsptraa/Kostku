b76dcb0a35243d11e5d439c7ac325982ded1dbb2
adityputr <adityputr45@gmail.com>
2023-11-07 15:39:49 +0700

    Menambahkan onCreateViewHolder, getItemCount dan onBindViewHolder

--- app/src/main/java/com/example/kostku/Adapter/Adapter.kt
+++ app/src/main/java/com/example/kostku/Adapter/Adapter.kt
@@ -1,4 +1,40 @@
 package com.example.kostku.Adapter
 
-class Adapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kostku.Model.User
+import com.example.kostku.R
+
+class Adapter : RecyclerView.Adapter<Adapter.MyViewHolder>() {
+
+    private val kostList = ArrayList<User>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.user_item, parent, false
+        )
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return kostList.size
+    }
+    fun updateKostList(kostList: List<User>){
+        this.kostList.clear()
+        this.kostList.addAll(kostList)
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = kostList[position]
+
+        holder.namaKost.text = currentItem.namaKos
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val namaKost : TextView = itemView.findViewById(R.id.tvNamaKos)
+    }
 }
--- app/src/main/java/com/example/kostku/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/kostku/Fragment/HomeFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.models.SlideModel
+import com.example.kostku.Adapter.Adapter
+import com.example.kostku.Model.UserViewModel
 import com.example.kostku.R
+
+private lateinit var viewModel : UserViewModel
+private lateinit var userRecyclerView : RecyclerView
+lateinit var adapter: Adapter
 
 class HomeFragment : Fragment() {
     val imageList = ArrayList<SlideModel>()
@@ -28,5 +37,21 @@ class HomeFragment : Fragment() {
         imageSlider.setImageList(imageList)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userRecyclerView = view.findViewById(R.id.recyclerView)
+        userRecyclerView.layoutManager = LinearLayoutManager(context)
+        userRecyclerView.setHasFixedSize(true)
+        adapter = Adapter()
+        userRecyclerView.adapter = adapter
+
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        viewModel.allKost.observe(viewLifecycleOwner, {
+            adapter.updateKostList(it)
+        })
     }
 }
