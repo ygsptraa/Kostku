e0224167d1fe9a43c2fd240a37359dded76df49a
salmawahyun <salmawahyun@gmail.com>
2023-11-09 23:44:31 +0700

    menambahkan kode kode tersebut untuk mengolah layout profil. setOnClickListener untuk mengklik gambar camera

--- app/src/main/java/com/example/kostku/Fragment/ProfilFragment.kt
+++ app/src/main/java/com/example/kostku/Fragment/ProfilFragment.kt
@@ -1,17 +1,55 @@
 package com.example.kostku.Fragment
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kostku.R
+import java.io.ByteArrayOutputStream
 
 class ProfilFragment : Fragment() {
+
+    companion object{
+        const val REQUEST_CAMERA = 100
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ivProfile.setOnClickListener{
+            intentCamera()
+        }
+    }
+
+    private fun intentCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {intent ->
+            activity?.packageManager?.let {
+                intent.resolveActivity(it).also {
+                    startActivityForResult(intent, REQUEST_CAMERA )
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+            uploadImage(imgBitmap)
+        }
+    }
+
+    private fun uploadImage(imgBitmap: Bitmap) {
+
     }
 }
