9ef78845c11e41eb57af8628531f513c690088a6
salmawahyun <salmawahyun@gmail.com>
2023-12-16 10:53:40 +0700

    Mengimplementasikan fungsi registrasi dan validasi input pengguna sebelum melakukan registrasi dengan firebase authentication.

--- app/src/main/java/com/example/kostku/RegisterActivity.kt
+++ app/src/main/java/com/example/kostku/RegisterActivity.kt
@@ -3,14 +3,19 @@ package com.example.kostku
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Patterns
 import android.widget.Toast
 import com.example.kostku.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRegisterBinding
+    private lateinit var databaseReference: DatabaseReference
     lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,54 +25,44 @@ class RegisterActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
         //Menambahkan clicklistener pada button login
-        binding.btnLogin.setOnClickListener{
+        binding.btnLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
         binding.btnSign.setOnClickListener {
             val email = binding.editEmailRegister.text.toString()
             val password = binding.editPasswordRegister.text.toString()
-            val name = binding.editNameRegister.text.toString()
+            val userName = binding.editNameRegister.text.toString()
+
+            if (TextUtils.isEmpty(userName)) {
+                Toast.makeText(applicationContext, "Username harus diisi", Toast.LENGTH_SHORT)
+                    .show()
+            }
             //Validasi email
-            if (email.isEmpty()){
+            if (email.isEmpty()) {
                 binding.editEmailRegister.error = "Email Harus diisi"
                 binding.editEmailRegister.requestFocus()
                 return@setOnClickListener
             }
             //validasi email tidak sesuai
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 binding.editEmailRegister.error = "Email tidak valid"
                 binding.editPasswordRegister.requestFocus()
                 return@setOnClickListener
             }
             //Validasi password
-            if(password.isEmpty()) {
+            if (password.isEmpty()) {
                 binding.editPasswordRegister.error = "Password Harus Diisi"
                 binding.editPasswordRegister.requestFocus()
                 return@setOnClickListener
             }
             //validasi password
-            if(password.length < 6){
+            if (password.length < 6) {
                 binding.editPasswordRegister.error = "Password Minimal 6 Karakter"
                 binding.editPasswordRegister.requestFocus()
                 return@setOnClickListener
             }
-            RegisterFirebase(email,password)
-        }
-    }
-
-    //melakukan registrasi pengguna dengan Firebase Authentication, dan memberikan umpan balik kepada pengguna apakah registrasi berhasil atau jika terjadi kesalahan.
-    private fun RegisterFirebase(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email,password)
-        .addOnCompleteListener(this){
-            if (it.isSuccessful){
-                Toast.makeText(this,"Register Berhasil", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this,LoginActivity::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
-            }
+            RegisterFirebase(userName, email, password)
         }
     }
 }
-
